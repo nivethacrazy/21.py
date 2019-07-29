f65771359290d393b9b246faa82edf36c61c4dea
nivethacrazy <53429920+nivethacrazy@users.noreply.github.com>
2019-07-29 16:18:18 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,9 @@
+
+  c = input("Please Enter Your Own Character : ")
+
+
+   if(c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u' or c == 'A'
+            or c == 'E' or c == 'I' or c == 'O' or c == 'U'):
+            print("The Given Character ", c, "is a Vowel")
+      else:
+          print("The Given Character ", c, "is a Consonant")
